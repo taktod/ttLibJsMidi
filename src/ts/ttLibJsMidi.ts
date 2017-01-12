5dd8a7641bd41c2ff2867fabfd5587f53d03c40a
taktod <poepoemix@hotmail.com>
2017-01-12 09:18:38 +0900

    effectorとsoundfontのaudioBufferも外部参照できるようにしておく。

--- src/ts/ttLibJsMidi.ts
+++ src/ts/ttLibJsMidi.ts
@@ -1,6 +1,12 @@
 import * as mid from "./midi/midi";
+import * as efc from "./midi/effector";
+import * as sft from "./midi/soundfont";
 
 export namespace midi {
   export class MIDI extends mid.MIDI{};
+  export class Effetor extends efc.Effector{};
+  export class Soundfont extends sft.Soundfont{};
 }
 export class MIDI extends mid.MIDI{};
+export class Effetor extends efc.Effector{};
+export class Soundfont extends sft.Soundfont{};
